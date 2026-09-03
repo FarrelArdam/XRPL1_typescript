accc91b20f403b9a1d2141cd106a32e37781d881
unknown <farrelardam@gmail.com>
2026-09-03 08:48:36 +0700

    done

--- src/05.1-callback-function/tasks/task-05.ts
+++ src/05.1-callback-function/tasks/task-05.ts
@@ -59,21 +59,43 @@ type ShippingCategoryOrder = Order & { shippingStatus: SHIPPING_CATEGORY }
 function getPaymentStatus(selectedOrder: Order): PaymentStatusOrder {
     // implement to determine payment status order
     // this function return order data within status or order
-    return;
+    const paymentstatus = selectedOrder.paid
+    let orderstatus: ORDER_STATUS = `UNPAID`
+    if (paymentstatus) {
+        orderstatus = `PAID`
+    }
+    return {
+        ...selectedOrder, 
+        status: orderstatus  
+    };
 }
 
 
 function getShippingCategory(selectedOrder: Order): ShippingCategoryOrder {
     // implement to determine shipping category of order
     // this function return order data within shipping cateogory
-    return;
+    const ShippingStatus = selectedOrder.total
+    let shippingStat: SHIPPING_CATEGORY = `ECONOMY SHIPPING`
+    if (ShippingStatus >= 1500000) {
+        shippingStat = `FREE SHIPPING`
+    } else if (ShippingStatus >= 500000) {
+        shippingStat = `STANDARD SHIPPING`
+    }
+    return {
+        ...selectedOrder, shippingStatus: shippingStat
+    };
 }
 
 function processOrder<T>(
     arr: Order[],
     callback: (order: Order) => T): T[] {
     // implement for callback
-    return;
+    const results: T[] = []
+    for (let index = 0; index < arr.length; index++) {
+        const result = callback(arr[index]);
+        results.push(result)  
+    }
+    return results;
 }
 
 const orderWithPaymentStatus = processOrder(orders, getPaymentStatus)
